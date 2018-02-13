3ce0f3414df0cd7b3d852521cfa3f0fc56ebd6c0
ENGINEER34-NB\PICO <yuwapongporn0131@gmail.com>
2018-02-13 10:55:09 +0700

    Create Migration
    Create Migration product;
    Create Migration category;

---
+++ database/migrations/2018_02_13_032014_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('pid');
+            $table->string('prodname');
+            $table->string('description');
+            $table->decimal('price', 6, 2);
+            $table->integer('cateid');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
